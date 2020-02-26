4942336cfbe0292d831fdd7357c5b20bfb5d5e36
Memo Khoury <majd.khoury93@gmail.com>
2020-02-26 01:45:54 -0500

    feat: added user schema with basic pw encryption

---
+++ model/user.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+const uuidv1 = require('uuid/v1');
+const crypto = require('crypto');
+/**
+ * This is the user schema! Not much to worry about regarding name, email and created. But the hashed
+ * password is interesting. The user enters their password in a 'virtual' field, which then takes that 
+ * input and hashes it with a randomly generated salt. The result is then stored in the 'hashed_password'
+ * field.  
+ */
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: "Name is required",
+        minlength: 2,
+        maxlength: 100
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    hashed_password: {
+        type: String,
+        required: true,
+    },
+    salt: String,
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: Date
+})
+
+userSchema.virtual('password')
+    .set((password) => {
+        // create temporary variable _password
+        this._password = password;
+        // generate a salt
+        this.salt = uuidv1();
+        // encrypt the password using the method encryptPassword which belongs to the user schema
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(() => {
+        return this.password;
+    })
+
+//#region methods
+userSchema.methods = {
+    /**
+     * encrypts the password with a string argument of password. If there's no password, it returns 
+     * null. otherwise, it tries to createHmac() with the this.salt generated earlier above. 
+     * After that, it updates the password.
+     */
+    encryptPassword: (password) => {
+        if (!password) return "";
+        try {
+            return crypto.createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex')
+        }
+        catch (err) {
+            return "Password could not be encrypted!";
+        }
+    }
+}
+
+module.exports = mongoose.model("User", userSchema);
--- routes/post.js
+++ routes/post.js
@@ -1,14 +1,13 @@
 // Third party imports
-const express = require('express')
-
+const express = require('express');
 // Internal imports
-const postController = require('../controllers/post-controller')
+const { getPosts, createPost } = require('../controllers/post-controller')
 
 const router = express.Router();
 
 // Root route gets all the posts from the post controller
-router.get('/', postController.getPosts);
-router.post('/post', postController.createPost)
+router.get('/', getPosts);
+router.post('/post', createPost)
 
 // We export the router that we previously initialized in order to use it in the app.get() from the main page
 module.exports = router;
